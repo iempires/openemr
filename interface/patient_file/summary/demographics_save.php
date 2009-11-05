0507288414a524fd17a6c7af21a30cd2ca240f0d
bradymiller <bradymiller>
2009-11-05 07:01:19 +0000

    deal with escape characters, magic quotes, and prevent sql-injection

--- interface/patient_file/summary/demographics_save.php
+++ interface/patient_file/summary/demographics_save.php
@@ -1,6 +1,22 @@
-<?
+<?php
 include_once("../../globals.php");
 include_once("$srcdir/patient.inc");
+include_once("$srcdir/acl.inc");
+include_once("$srcdir/options.inc.php");
+include_once("$srcdir/formdata.inc.php");
+
+// Check authorization.
+$thisauth = acl_check('patients', 'demo');
+if ($pid) {
+  if ($thisauth != 'write')
+    die(xl('Updating demographics is not authorized.'));
+  $tmp = getPatientData($pid, "squad");
+  if ($tmp['squad'] && ! acl_check('squads', $tmp['squad']))
+    die(xl('You are not authorized to access this squad.'));
+} else {
+  if ($thisauth != 'write' && $thisauth != 'addonly')
+    die(xl('Adding demographics is not authorized.'));
+}
 
 foreach ($_POST as $key => $val) {
   if ($val == "MM/DD/YYYY") {
@@ -8,166 +24,137 @@ foreach ($_POST as $key => $val) {
   }
 }
 
-if ($_POST["sex"] == "Unselected") {
-	$var_sex = "";
-} else {
-	$var_sex = $_POST["sex"];
+// Update patient_data and employer_data:
+//
+$newdata = array();
+$newdata['patient_data']['id'] = $_POST['db_id'];
+$fres = sqlStatement("SELECT * FROM layout_options " .
+  "WHERE form_id = 'DEM' AND uor > 0 AND field_id != '' " .
+  "ORDER BY group_name, seq");
+while ($frow = sqlFetchArray($fres)) {
+  $data_type = $frow['data_type'];
+  $field_id  = $frow['field_id'];
+  // $value  = '';
+  $colname = $field_id;
+  $table = 'patient_data';
+  if (strpos($field_id, 'em_') === 0) {
+    $colname = substr($field_id, 3);
+    $table = 'employer_data';
+  }
+
+  // if (isset($_POST["form_$field_id"])) $value = $_POST["form_$field_id"];
+  $value = get_layout_form_value($frow);
+
+  $newdata[$table][$colname] = $value;
 }
+updatePatientData($pid, $newdata['patient_data']);
+updateEmployerData($pid, $newdata['employer_data']);
 
-if ($_POST{dob} != "") {
-	$dob = $_POST["dob"];
-} else {
-	$dob = "";
-}
-
-$finrev = fixDate($_POST["financial_review"]);
-
-newPatientData(
-  $_POST["db_id"],
-  $_POST["title"],
-  $_POST["fname"],
-  $_POST["lname"],
-  $_POST["mname"],
-  $var_sex,
-  $dob,
-  $_POST["street"],
-  $_POST["postal_code"],
-  $_POST["city"],
-  $_POST["state"],
-  $_POST["country_code"],
-  $_POST["ss"],
-  $_POST["occupation"],
-  $_POST["phone_home"],
-  $_POST["phone_biz"],
-  $_POST["phone_contact"],
-  $_POST["status"],
-  $_POST["contact_relationship"],
-  $_POST["referrer"],
-  $_POST["referrerID"],
-  $_POST["email"],
-  strtolower($_POST["language"]),
-  $_POST["ethnoracial"],
-  $_POST["interpretter"],
-  $_POST["migrantseasonal"],
-  $_POST["family_size"],
-  $_POST["monthly_income"],
-  $_POST["homeless"],
-  $finrev,
-  $_POST["pubpid"],
-  $pid,
-  $_POST["providerID"],
-  $_POST["genericname1"],
-  $_POST["genericval1"],
-  $_POST["genericname2"],
-  $_POST["genericval2"],
-  $_POST["phone_cell"],
-  $_POST["hipaa_mail"],
-  $_POST["hipaa_voice"],
-  $_POST["squad"]
-);
-
-newEmployerData(
-  $pid,
-  $_POST["ename"],
-  $_POST["estreet"],
-  $_POST["epostal_code"],
-  $_POST["ecity"],
-  $_POST["estate"],
-  $_POST["ecountry"]
-);
-
-$i1dob = fixDate($_POST["i1subscriber_DOB"]);
+$i1dob = fixDate(formData("i1subscriber_DOB"));
+$i1date = fixDate(formData("i1effective_date"), date('Y-m-d'));
 
 newInsuranceData(
   $pid,
   "primary",
-  $_POST["i1provider"],
-  $_POST["i1policy_number"],
-  $_POST["i1group_number"],
-  $_POST["i1plan_name"],
-  $_POST["i1subscriber_lname"],
-  $_POST["i1subscriber_mname"],
-  $_POST["i1subscriber_fname"],
-  $_POST["i1subscriber_relationship"],
-  $_POST["i1subscriber_ss"],
+  formData("i1provider"),
+  formData("i1policy_number"),
+  formData("i1group_number"),
+  formData("i1plan_name"),
+  formData("i1subscriber_lname"),
+  formData("i1subscriber_mname"),
+  formData("i1subscriber_fname"),
+  formData("form_i1subscriber_relationship"),
+  formData("i1subscriber_ss"),
   $i1dob,
-  $_POST["i1subscriber_street"],
-  $_POST["i1subscriber_postal_code"],
-  $_POST["i1subscriber_city"],
-  $_POST["i1subscriber_state"],
-  $_POST["i1subscriber_country"],
-  $_POST["i1subscriber_phone"],
-  $_POST["i1subscriber_employer"],
-  $_POST["i1subscriber_employer_street"],
-  $_POST["i1subscriber_employer_city"],
-  $_POST["i1subscriber_employer_postal_code"],
-  $_POST["i1subscriber_employer_state"],
-  $_POST["i1subscriber_employer_country"],
-  $_POST['i1copay'],
-  $_POST['i1subscriber_sex']
+  formData("i1subscriber_street"),
+  formData("i1subscriber_postal_code"),
+  formData("i1subscriber_city"),
+  formData("form_i1subscriber_state"),
+  formData("form_i1subscriber_country"),
+  formData("i1subscriber_phone"),
+  formData("i1subscriber_employer"),
+  formData("i1subscriber_employer_street"),
+  formData("i1subscriber_employer_city"),
+  formData("i1subscriber_employer_postal_code"),
+  formData("form_i1subscriber_employer_state"),
+  formData("form_i1subscriber_employer_country"),
+  formData('i1copay'),
+  formData('form_i1subscriber_sex'),
+  $i1date,
+  formData('i1accept_assignment')
 );
 
-$i2dob = fixDate($_POST["i2subscriber_DOB"]);
+$i2dob = fixDate(formData("i2subscriber_DOB"));
+$i2date = fixDate(formData("i2effective_date"), date('Y-m-d'));
 
 newInsuranceData(
   $pid,
   "secondary",
-  $_POST["i2provider"],
-  $_POST["i2policy_number"],
-  $_POST["i2group_number"],
-  $_POST["i2plan_name"],
-  $_POST["i2subscriber_lname"],
-  $_POST["i2subscriber_mname"],
-  $_POST["i2subscriber_fname"],
-  $_POST["i2subscriber_relationship"],
-  $_POST["i2subscriber_ss"],
+  formData("i2provider"),
+  formData("i2policy_number"),
+  formData("i2group_number"),
+  formData("i2plan_name"),
+  formData("i2subscriber_lname"),
+  formData("i2subscriber_mname"),
+  formData("i2subscriber_fname"),
+  formData("form_i2subscriber_relationship"),
+  formData("i2subscriber_ss"),
   $i2dob,
-  $_POST["i2subscriber_street"],
-  $_POST["i2subscriber_postal_code"],
-  $_POST["i2subscriber_city"],
-  $_POST["i2subscriber_state"],
-  $_POST["i2subscriber_country"],
-  $_POST["i2subscriber_phone"],
-  $_POST["i2subscriber_employer"],
-  $_POST["i2subscriber_employer_street"],
-  $_POST["i2subscriber_employer_city"],
-  $_POST["i2subscriber_employer_postal_code"],
-  $_POST["i2subscriber_employer_state"],
-  $_POST["i2subscriber_employer_country"],
-  $_POST['i2copay'],
-  $_POST['i2subscriber_sex']
+  formData("i2subscriber_street"),
+  formData("i2subscriber_postal_code"),
+  formData("i2subscriber_city"),
+  formData("form_i2subscriber_state"),
+  formData("form_i2subscriber_country"),
+  formData("i2subscriber_phone"),
+  formData("i2subscriber_employer"),
+  formData("i2subscriber_employer_street"),
+  formData("i2subscriber_employer_city"),
+  formData("i2subscriber_employer_postal_code"),
+  formData("form_i2subscriber_employer_state"),
+  formData("form_i2subscriber_employer_country"),
+  formData('i2copay'),
+  formData('form_i2subscriber_sex'),
+  $i2date,
+  formData('i2accept_assignment')
 );
 
-$i3dob = fixDate($_POST["i3subscriber_DOB"]);
+$i3dob  = fixDate(formData("i3subscriber_DOB"));
+$i3date = fixDate(formData("i3effective_date"), date('Y-m-d'));
 
 newInsuranceData(
   $pid,
   "tertiary",
-  $_POST["i3provider"],
-  $_POST["i3policy_number"],
-  $_POST["i3group_number"],
-  $_POST["i3plan_name"],
-  $_POST["i3subscriber_lname"],
-  $_POST["i3subscriber_mname"],
-  $_POST["i3subscriber_fname"],
-  $_POST["i3subscriber_relationship"],
-  $_POST["i3subscriber_ss"],
+  formData("i3provider"),
+  formData("i3policy_number"),
+  formData("i3group_number"),
+  formData("i3plan_name"),
+  formData("i3subscriber_lname"),
+  formData("i3subscriber_mname"),
+  formData("i3subscriber_fname"),
+  formData("form_i3subscriber_relationship"),
+  formData("i3subscriber_ss"),
   $i3dob,
-  $_POST["i3subscriber_street"],
-  $_POST["i3subscriber_postal_code"],
-  $_POST["i3subscriber_city"],
-  $_POST["i3subscriber_state"],
-  $_POST["i3subscriber_country"],
-  $_POST["i3subscriber_phone"],
-  $_POST["i3subscriber_employer"],
-  $_POST["i3subscriber_employer_street"],
-  $_POST["i3subscriber_employer_city"],
-  $_POST["i3subscriber_employer_postal_code"],
-  $_POST["i3subscriber_employer_state"],
-  $_POST["i3subscriber_employer_country"],
-  $_POST['i3copay'],
-  $_POST['i3subscriber_sex']
+  formData("i3subscriber_street"),
+  formData("i3subscriber_postal_code"),
+  formData("i3subscriber_city"),
+  formData("form_i3subscriber_state"),
+  formData("form_i3subscriber_country"),
+  formData("i3subscriber_phone"),
+  formData("i3subscriber_employer"),
+  formData("i3subscriber_employer_street"),
+  formData("i3subscriber_employer_city"),
+  formData("i3subscriber_employer_postal_code"),
+  formData("form_i3subscriber_employer_state"),
+  formData("form_i3subscriber_employer_country"),
+  formData('i3copay'),
+  formData('form_i3subscriber_sex'),
+  $i3date,
+  formData('i3accept_assignment')
 );
 
-include_once("patient_summary.php");
+if ($GLOBALS['concurrent_layout']) {
+ include_once("demographics.php");
+} else {
+ include_once("patient_summary.php");
+}
 ?>
